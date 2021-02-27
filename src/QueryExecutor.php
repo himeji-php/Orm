bbf5db02592ab429892d8d5f115e3ee8ef358920
ketchums <hicks6408@gmail.com>
2021-02-27 00:00:31 +0000

    Small updates

---
+++ src/QueryExecutor.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Himeji\Orm;
+
+class QueryExecutor
+{
+    private $connection;
+
+    public function __construct($host, $username, $password, $database, $port) {
+        $this->switchConnection($host, $username, $password, $database, $port);
+    }
+
+    public function switchConnection($host, $username, $password, $database, $port) {
+        $this->connection = mysqli_connect($host, $username, $password, $database, $port);
+    }
+
+    public function execute($command) : bool|\mysqli_result {
+        $this->connection->query($command);
+    }
+}
